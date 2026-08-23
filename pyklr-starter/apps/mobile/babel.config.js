c40be5d09407cc8dd7e0f554209aba2e12880c8f
Merrick Lee <princemlee@aol.com>
2026-08-23 08:53:31 -0400

    chore: migrate NativeWind v4→v5, Tailwind CSS v3→v4, remove css-interop patch, accept all SDK 54 dep pins

--- pyklr-starter/apps/mobile/babel.config.js
+++ pyklr-starter/apps/mobile/babel.config.js
@@ -1,10 +1,7 @@
 module.exports = function (api) {
   api.cache(false); // Force cache invalidation!
   return {
-    presets: [
-      ['babel-preset-expo', { jsxImportSource: 'nativewind' }],
-      'nativewind/babel',
-    ],
+    presets: ['babel-preset-expo'],
     plugins: [
       'react-native-worklets/plugin', // Reanimated 4 uses worklets plugin
     ],
--- pyklr-starter/apps/mobile/metro.config.js
+++ pyklr-starter/apps/mobile/metro.config.js
@@ -15,4 +15,4 @@ config.resolver.nodeModulesPaths = [
 ];
 config.resolver.disableHierarchicalLookup = true;
 
-module.exports = withNativeWind(config, { input: './global.css' });
+module.exports = withNativeWind(config);
